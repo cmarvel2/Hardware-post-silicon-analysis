5fc4d4ecedd1c4cbb9b855867ebdb68dd5e9ef41
cmarvel2 <cmarvel@umich.edu>
2025-10-20 00:52:46 -0400

    Implemeted get_gpu_info, to return the GPU name as a key, and values like memory total, memory used, gpu load, memory utilization, and temperature

--- System_data.py
+++ System_data.py
@@ -1,8 +1,8 @@
 import psutil
-import GPUtil
 import cpuinfo
-import pprint
+import GPUtil
 import pandas as pd
+import os
 
 
 class Sysdata:
@@ -22,9 +22,20 @@ class Sysdata:
         def sortmem(e):
             return e['memory_percent']
         
-        Processlist.sort(key=sortmem, reverse=True)[]
+        Processlist.sort(key=sortmem, reverse=True)
 
-        print(Processlist)
+        return Processlist[:5]
+    
+    @classmethod
+    def get_gpu_info(cls):
+        gpuinfo = []
+
+        gpus = GPUtil.getGPUs()
+        if not gpus:
+            print("Nvidia GPU Detetct Only")
+        for i in gpus:
+            gpuinfo.append({i.name: (i.memoryTotal, i.memoryUsed, i.load, f"{round(i.memoryUtil*100)}%", i.temperature)})
+        return gpuinfo
 
 
     def get_telemetry(self):
@@ -35,10 +46,18 @@ class Sysdata:
                 "Logical Processors": [psutil.cpu_count()],
                 "CPU Utilization": [f"{psutil.cpu_percent()}%"],
                 "CPU Frequency": [psutil.cpu_freq().current],
-                "Total Memory in GB": [round(psutil.virtual_memory()[0]/1024**3,1)],
-                "Memory Utilization in GB": [round(psutil.virtual_memory()[3]/1024**3,1)],
-                "Memory Utilization": [f"{psutil.virtual_memory()[2]}%"],
-                "Disk Utilization":  [psutil.disk_io_counters()],
+                "Total Memory in GB": [round(psutil.virtual_memory().total/1024**3,1)],
+                "Memory Utilization in GB": [round(psutil.virtual_memory().used/1024**3,1)],
+                "Memory Utilization": [f"{psutil.virtual_memory().percent}%"],
+                "Disk Capacity in GB": [round(psutil.disk_usage("C:\\").total/1024**3)],
+                "Disk Utilization in GB": [round(psutil.disk_usage("C:\\").used/1024**3)],
+                "Disk Utilization": [f"{psutil.disk_usage("c:\\").percent}%"],
+                #"Bytes sent in KB": [psutil.net_io_counters(pernic=True)['lo'][0]/1024],
+                #"Bytes recived in KB": [psutil.net_io_counters(pernic=True)['lo'][1]/1024]
+                "GPU": [Sysdata.get_gpu_info()],
+                "GPU Utilization in GB": [Sysdata.get_gpu_info()],
+                "Total GPU Memory in GB": [Sysdata.get_gpu_info()],
+                "GPU Memory Utilization": [Sysdata.get_gpu_info()]
         }
 
         df =pd.DataFrame(data)
@@ -46,4 +65,8 @@ class Sysdata:
         return df
 
 system = Sysdata()
-print(system.get_top_processes())
+#print(system.get_top_processes())
+print(system.get_telemetry())
+
+
+
