90920de5b21a4e9fe4cb4dae4dc945458e8a5c11
cmarvel2 <cmarvel@umich.edu>
2025-10-20 01:49:16 -0400

    Shifted priorities of the data get_telemetry function to primariy focus on formatting the data and for get_gpu_info to do calculations, Reformatting data to change areas where there are repeated calls to the same function such as givng disk_usage a variable to access instead of calling it three times and so on.

--- System_data.py
+++ System_data.py
@@ -28,17 +28,33 @@ class Sysdata:
     
     @classmethod
     def get_gpu_info(cls):
-        gpuinfo = []
+        gpuinfo = {
+            "Names": [], 
+            "TotalMemory": [], 
+             "UsedMemory": [], 
+            "Utilization": [], 
+            "MemUtilization": [], 
+            "Temperature": []
+        }
 
         gpus = GPUtil.getGPUs()
         if not gpus:
-            print("Nvidia GPU Detetct Only")
-        for i in gpus:
-            gpuinfo.append({i.name: (i.memoryTotal, i.memoryUsed, i.load, f"{round(i.memoryUtil*100)}%", i.temperature)})
+            print("Currently Only Detects Nvidia GPUs")
+        for num, gpu in enumerate(gpus):
+            gpuinfo["Names"].append(gpu.name)
+            gpuinfo["TotalMemory"].append(str(float(round(gpu.memoryTotal /1024))))
+            gpuinfo["UsedMemory"].append(str(round(gpu.memoryUsed /1024,1)))
+            gpuinfo["Utilization"].append(str(round(gpu.load)))
+            gpuinfo["MemUtilization"].append(str(round(gpu.memoryUtil*100)))
+            gpuinfo["Temperature"].append(str(round(gpu.temperature)))
+            
         return gpuinfo
 
 
     def get_telemetry(self):
+        gpu_info =  Sysdata.get_gpu_info()
+        gpuname = ",".join(gpu_info.get("Names", "No GPU Detected"))
+
 
         data = {
                 #"CPU": [cpuinfo.get_cpu_info()['brand_raw']],
@@ -46,19 +62,21 @@ class Sysdata:
                 "Logical Processors": [psutil.cpu_count()],
                 "CPU Utilization": [f"{psutil.cpu_percent()}%"],
                 "CPU Frequency": [psutil.cpu_freq().current],
-                "Total Memory in GB": [round(psutil.virtual_memory().total/1024**3,1)],
-                "Memory Utilization in GB": [round(psutil.virtual_memory().used/1024**3,1)],
+                "Total Memory": [f"{round(psutil.virtual_memory().total/1024**3,1)} GB"],
+                "Memory Utilization": [f"{round(psutil.virtual_memory().used/1024**3,1)} GB"],
                 "Memory Utilization": [f"{psutil.virtual_memory().percent}%"],
-                "Disk Capacity in GB": [round(psutil.disk_usage("C:\\").total/1024**3)],
-                "Disk Utilization in GB": [round(psutil.disk_usage("C:\\").used/1024**3)],
+                "Disk Capacity": [f"{round(psutil.disk_usage("C:\\").total/1024**3)} GB"],
+                "Disk Utilization": [f"{round(psutil.disk_usage("C:\\").used/1024**3)} GB"],
                 "Disk Utilization": [f"{psutil.disk_usage("c:\\").percent}%"],
                 #"Bytes sent in KB": [psutil.net_io_counters(pernic=True)['lo'][0]/1024],
                 #"Bytes recived in KB": [psutil.net_io_counters(pernic=True)['lo'][1]/1024]
-                "GPU": [Sysdata.get_gpu_info()],
-                "GPU Utilization in GB": [Sysdata.get_gpu_info()],
-                "Total GPU Memory in GB": [Sysdata.get_gpu_info()],
-                "GPU Memory Utilization": [Sysdata.get_gpu_info()]
-        }
+                "GPU": [",".join(gpu_info.get("Names", "No GPU Detected"))],
+                "GPU Utilization": [f"{",".join(gpu_info.get("Utilization", "0"))}%"],
+                "Total GPU Memory": [f"{",".join(gpu_info.get("TotalMemory", "0"))} GB"],
+                "Used GPU Memory": [f"{",".join(gpu_info.get("UsedMemory", "0"))} GB"],
+                "GPU Memory Utilization": [f"{",".join(gpu_info.get("MemUtilization", "0"))}%"],
+                "GPU Temperature": [f"{",".join(gpu_info.get("Temperature", "0"))} C"]
+}
 
         df =pd.DataFrame(data)
         df=df.to_string()
