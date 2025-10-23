b253e126f0507e4eadfba08291f15a8bc936547e
cmarvel2 <cmarvel@umich.edu>
2025-10-23 12:31:57 -0400

    Finished data_manager file, added try except statemeent for when proceses due to error statement "File "C:\Users\corey\Anaconda3\Lib\site-packages\psutil\__init__.py", line 361, in _init     raise NoSuchProcess(pid, msg=msg) from None psutil.NoSuchProcess: process PID not found (pid=38512)", Unable to replicate, Implementing framework for data_manager file and adding timestamp to dataframe before sending it to a database/file

--- System_data.py
+++ System_data.py
@@ -1,33 +1,55 @@
-import psutil, GPUtil, cpuinfo, platform, subprocess
-import pandas as pd
+import psutil, GPUtil, cpuinfo, platform, subprocess, pprint
+from pyuac import main_requires_admin
 
 
 class Sysdata:
     def get_top_processes(self):
 
         Processlist = []
-        for p in psutil.process_iter(['name']):
-            process= psutil.Process(pid=p.pid)
-            Processlist.append(process.as_dict(attrs= ['name','cpu_percent','memory_percent'] ))
+        UniqProcesslist =[]
         
+
+        for p in psutil.process_iter(['name']):
+            try:
+                process = psutil.Process(pid=p.pid)
+                Processlist.append(process.as_dict(attrs= ['name','cpu_percent','memory_percent'] ))
+            except psutil.NoSuchProcess(pid=p.pid):
+                pass
+
         for process in Processlist:
             for k,v in process.items():
                 if k == 'memory_percent' and k:
-                     process[k] = round(v,1)
+                      process[k] = round(v,1)
+
+        for process1 in Processlist: 
+                
+                names_in_uniq_list = [p['name'] for p in UniqProcesslist]
+
+                if process1['name'] not in names_in_uniq_list:
+                    UniqProcesslist.append(process1)
+                else:
+                    for process2 in UniqProcesslist:
+                        if process1['name'] == process2['name']:
+                            process2['memory_percent'] += process1.get('memory_percent',0)
+                            process2['cpu_percent'] += process1.get('cpu_percent',0)
+                            break
+        
+        #for num, processname in enumerate(UniqProcesslist)
+
         
         def sortmem(mem):
             return mem['memory_percent']
         
-        Processlist.sort(key=sortmem, reverse=True)
-
-        return Processlist
+        UniqProcesslist.sort(key=sortmem, reverse=True)
+        print(UniqProcesslist)
+        return UniqProcesslist[:5]
     
     @classmethod
     def get_gpu_info(cls):
         gpuinfo = {
             "Names": [], 
             "TotalMemory": [], 
-             "UsedMemory": [], 
+            "UsedMemory": [], 
             "Utilization": [], 
             "MemUtilization": [], 
             "Temperature": []
@@ -47,31 +69,42 @@ class Sysdata:
         return gpuinfo
     
     @classmethod
+    #@main_requires_admin
     def system_specif_functs(self):
         Sysfunctlist = []
         CPUtemp = None
         CPUfreq = None
         
+        
         if platform.system() == "Windows":
-            command = r"""
+            GHzcommand = r"""
         $MaxClockSpeed = (Get-CimInstance CIM_Processor).MaxClockSpeed
         $ProcessorPerformance = (Get-Counter -Counter "\Processor Information(_Total)\% Processor Performance").CounterSamples.CookedValue
         $CurrentClockSpeed = $MaxClockSpeed*($ProcessorPerformance/100)
         Write-Host $CurrentClockSpeed
 """
-            completed = subprocess.run(["powershell", "-command", command], capture_output=True)
-            Freq = float(completed.stdout) / 1000
+            GHzcompleted = subprocess.run(["powershell", "-command", GHzcommand], capture_output=True)
+            Freq = float(GHzcompleted.stdout) / 1000
             Sysfunctlist.append(round(Freq,2))
+
+            Cpucommand = r"""((Get-CimInstance MSAcpi_ThermalZoneTemperature -Namespace "root/wmi").CurrentTemperature / 10 - 273.15)
+""" 
+            Cpucompleted = subprocess.run(['powershell', "-command", Cpucommand], capture_output=True,)
+            ctemp = Cpucompleted.stdout
+            Sysfunctlist.append(ctemp.decode().strip())
+           
+            
+            
             
         elif platform.system() == "Linux":
-            print("Ltest")
+            Sysfunctlist.append(psutil.cpu_freq().current)
+            Sysfunctlist.append(psutil.sensors._temperatures()['coretemp'].current())
         else:
             CPUtemp = "Not Supported"
             Cpufreq = "Not Supported"
 
         for p in psutil.disk_partitions():
             Sysfunctlist.append(p.mountpoint)
-        print(Sysfunctlist)
         return Sysfunctlist
 
     def get_telemetry(self):
@@ -87,17 +120,15 @@ class Sysdata:
                 "CPU": [cpuinfo.get_cpu_info()['brand_raw']],
                 "Cores": [psutil.cpu_count(logical=False)],
                 "Logical Processors": [psutil.cpu_count()],
-                "CPU Utilization": [f"{psutil.cpu_percent()}%"],
-                "CPU Frequency": [psutil.cpu_freq().current],
-                "CPU Temperature":[f"{[cpu_temp]} C"],
+                "CPU Utilization": [f"{psutil.cpu_percent(interval=0.1)}%"],
+                "CPU Frequency": [f"{Sysfuncts[0]} GHz"],
+                #"CPU Temperature":[f"{[Sysfuncts[1]]} C"],
                 "Total System Memory": [f"{round(mem_info.total/1024**3,1)} GB"],
                 "Used System Memory": [f"{round(mem_info.used/1024**3,1)} GB"],
                 "Memory Utilization": [f"{mem_info.percent}%"],
                 "Total Disk Capacity": [f"{round(disk_info.total/1024**3)} GB"],
                 "Used Disk Capacity": [f"{round(disk_info.used/1024**3)} GB"],
                 "Disk Utilization": [f"{disk_info.percent}%"],
-                #"Bytes sent in KB": [psutil.net_io_counters(pernic=True)['lo'][0]/1024],
-                #"Bytes recived in KB": [psutil.net_io_counters(pernic=True)['lo'][1]/1024]
                 "GPU": [",".join(gpu_info.get("Names", "No GPU Detected"))],
                 "GPU Utilization": [f"{",".join(gpu_info.get("Utilization", "0"))}%"],
                 "Total GPU Memory": [f"{",".join(gpu_info.get("TotalMemory", "0"))} GB"],
@@ -110,9 +141,9 @@ class Sysdata:
         return data
 
 system = Sysdata()
-#pprint.pp(system.get_top_processes())
-#pprint.pp(system.get_telemetry())
-Sysdata.system_specif_functs()
+system.get_top_processes()
+system.get_telemetry()
+
 
 
 
--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,30 @@
 import pandas as pd
 import sqlite3
 from System_data import Sysdata
+from datetime import datetime
 
 class Data_formatter:
     
-    def __init__(self, system_data):
-        pass
+    def __init__(self, system_data, process_data):
+        self.system_data =  system_data
+        self.process_data = process_data
+
+    def System_telemetry_dataframe(self):
+        df = pd.DataFrame(self.system_data)
+
+        return df
+
+    def processor_data_dataframe(self):
+        df = pd.DataFrame(self.process_data)
+
+        return df
+
+system = Sysdata()
+systemdata= system.get_telemetry()
+processdata= system.get_top_processes()
+formatteddata = Data_formatter(systemdata,processdata)
+#print(formatteddata.System_telemetry_dataframe())
+#print(formatteddata.processor_data_dataframe())
+
+
+print(datetime.today())
