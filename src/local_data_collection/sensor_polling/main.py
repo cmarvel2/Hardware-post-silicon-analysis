82a5a157cd499765984d8f69520698117de517df
Corey Royster <cmarvel@umich.edu>
2026-09-06 22:22:07 -0400

    Logger called inside main.py, check buffer is now at the beginning of the loop and checksif the payload should be sent to azure blob storage or  it should continue adding to the buffer. This was due to potential situation where an error in upload blob  returning false could silently swallow an error and keep adding to the payload for the remainder of every run instead of raising an error which was also fixes with logging.exception now being put in the try except block
    test_id is now defined in main instead of upload_blob and is included in error messages
    
    To utilize hierarchical namespace in the azure dev container and implement date partitioning for efficiency uploaded files now create a folder for that day with "ingest_date{datefolder}/"

--- src/local_data_collection/sensor_polling/main.py
+++ src/local_data_collection/sensor_polling/main.py
@@ -1,37 +1,52 @@
 import time
 import concurrent.futures
+import logging
+import uuid
 
 from local_data_collection.sensor_polling.hardware_sensor_collectors import init_computer, get_cpu_data, get_gpu_data, get_memory_data
 from local_data_collection.sensor_polling.adls_payload import UploadMemory, adls_credentials, upload_blob
-from local_data_collection.utils.config_loader import load_conf
+from local_data_collection.utils import config_loader, logger
+
+logger.logging_setup()
 
 def main():
-    computer, hardwaretype = init_computer(cpu=True, gpu=True, motherboard=True, memory=True)
-    testing_configs = load_conf(r"C:\courses_and_personal_projects\hardware_data_pipeline\src\local_data_collection\conf\occt_software.yml")
-    end_time = time.time() + 60 * testing_configs["OCCT"]["stability_test"]["length_in_minutes"]
-    mainmemory = UploadMemory()
-    credential, env_mapping = adls_credentials()
+    test_id = str(uuid.uuid4())
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        while time.time() < end_time:
-            f1 = executor.submit(get_memory_data, computer, hardwaretype)
-            f2 = executor.submit(get_gpu_data, computer, hardwaretype)
-            f3 = executor.submit(get_cpu_data, computer, hardwaretype)
+    try:
+        computer, hardwaretype = init_computer(cpu=True, gpu=True, motherboard=True, memory=True)
+        testing_configs = config_loader.load_conf(r"C:\courses_and_personal_projects\hardware_data_pipeline\src\local_data_collection\conf\occt_software.yml")
+        end_time = time.time() + 60 * testing_configs["OCCT"]["stability_test"]["length_in_minutes"]
+        mainmemory = UploadMemory()
+        credential, env_mapping = adls_credentials()
 
-            memory_data = f1.result()
-            gpu_data = f2.result()
-            cpu_data = f3.result()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            while time.time() < end_time:
+                check_buffer_result = mainmemory.check_buffer(buffer_size_limit=200000)
+                if check_buffer_result == False:
+                    f1 = executor.submit(get_memory_data, computer, hardwaretype)
+                    f2 = executor.submit(get_gpu_data, computer, hardwaretype)
+                    f3 = executor.submit(get_cpu_data, computer, hardwaretype)
 
-            mainmemory.payload_buffer_add(filename=r"C:\courses_and_personal_projects\hardware_data_pipeline\src\local_data_collection\conf\occt_software.yml", cpu_payload=cpu_data, gpu_payload=gpu_data, memory_payload=memory_data)
+                    memory_data = f1.result()
+                    gpu_data = f2.result()
+                    cpu_data = f3.result()
 
-            check_buffer_result = mainmemory.check_buffer(buffer_size_limit=200000)
+                    mainmemory.payload_buffer_add(filename=r"C:\courses_and_personal_projects\hardware_data_pipeline\src\local_data_collection\conf\occt_software.yml", 
+                                                cpu_payload=cpu_data,
+                                                    gpu_payload=gpu_data, 
+                                                    memory_payload=memory_data,
+                                                    test_id=test_id)
+                else:
+                    upload_blob_result = upload_blob(credentials=credential, env_mapping=env_mapping, sensor_buffer=mainmemory.payload)
 
-            upload_blob_result = upload_blob(credentials=credential, env_mapping=env_mapping, buffer_complete=check_buffer_result, sensor_buffer=mainmemory.payload)
+                    mainmemory.clear_buffer(buffer_uploaded=upload_blob_result)
 
-            mainmemory.clear_buffer(buffer_uploaded=upload_blob_result)
-
-    if mainmemory.payload.snapshots:
-        upload_blob(credentials=credential, env_mapping=env_mapping, buffer_complete=True, sensor_buffer=mainmemory.payload)
+        if mainmemory.payload.snapshots:
+            upload_blob(credentials=credential, env_mapping=env_mapping, sensor_buffer=mainmemory.payload)
+    except Exception as e:
+        logging.exception(f"Error in pipeline run: {e} test id: {test_id}")
+        raise
 
 if __name__ == "__main__":
     main()
+        
